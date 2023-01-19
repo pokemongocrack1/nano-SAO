4109c866a69102cd1d89ae86bc005caa89ed07ba
pokemongocrack1 <118348261+pokemongocrack1@users.noreply.github.com>
2023-01-19 17:20:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23333,7 +23333,10 @@ class Q1 {
     get inSelfTeam() {
         return U.teamPlayers.size && U.team == this.owner.team
     }
-    get nick() { return this.owner._tripkey ? this._nick+" ["+this.owner._bfp.substr(0,4)+"]" : this._nick }
+    get nick() { 
+        console.log(this)
+        return this.owner._tripkey ? this._nick+" ["+this.owner._bfp.substr(0,4)+"]" : this._nick 
+    }
     set nick(t) {
         this._nick !== t && (this._nick = t, this.updateText(), this.pcells.forEach(i => i.setNick()))
     }
